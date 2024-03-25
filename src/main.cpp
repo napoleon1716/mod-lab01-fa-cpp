b9bbf16d3a7a9fbb772e20e20d38085373471fb4
napoleon1716 <119057003+napoleon1716@users.noreply.github.com>
2024-03-25 18:52:17 +0300

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,16 @@
 // Copyright 2022 UNN-IASR
 #include "fun.h"
+#include <iostream>
+#include <string>
 
 int main() {
+    std::string str;
+    std::cout << "Enter a string: ";
+    std::getline(std::cin, str);
+    const char* cstr = str.c_str();
+
+    std::cout << "faStr1: " << faStr1(cstr) << std::endl;
+    std::cout << "faStr2: " << faStr2(cstr) << std::endl;
+    std::cout << "faStr3: " << faStr3(cstr) << std::endl;
     return 0;
 }
