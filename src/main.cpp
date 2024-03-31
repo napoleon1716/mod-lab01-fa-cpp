02d70c9612354223502f6435e0d3928d0db045fe
napoleon1716 <119057003+napoleon1716@users.noreply.github.com>
2024-03-31 12:05:00 +0300

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,9 @@
 // Copyright 2022 UNN-IASR
+#include <iostream>
 #include "fun.h"
 
 int main() {
-    const char* cstr = "Gdyuey aauw w!";
-    
+    const char* cstr = "Hello, world!";
     std::cout << "faStr1: " << faStr1(cstr) << std::endl;
     std::cout << "faStr2: " << faStr2(cstr) << std::endl;
     std::cout << "faStr3: " << faStr3(cstr) << std::endl;
